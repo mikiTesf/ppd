dddd27e2c78afcc0417c9cf8995dcac5254f84f0
Mikyas Tesfamichael <mickyastesfamichael@gmail.com>
2020-10-07 11:37:15 +0300

    added more files to .gitignore; declared the required methods

--- pd.py
+++ pd.py
@@ -1,5 +1,10 @@
 from requests import get
 from datetime import datetime
+import re
+from sys import argv
+
+
+args = argv
 
 
 def zero_pad_month(month: str):
@@ -12,9 +17,27 @@ today = datetime.today()
 
 year = str(today.year)
 month = zero_pad_month(str(today.month))
-pub = 'mwb'
+pub = None
 ftype = 'JWPUB'
 lang = 'AM'
+cont = False
+
+
+def validate_args(
+        arg_year: str, arg_month: str, arg_pub: str,
+        arg_ftype: str, arg_lang: str, arg_cont: str):
+    # This line of comment has absolutely no purpose (love it or leave it)
+    match = re.match('year=(\\d{4})', arg_year)
+    year = match.group(1)
+
+
+def get_resource_link(url: str):
+    response = get(url)
+    pass
+
+
+def download_publication(url: str):
+    pass
 
 
 request_url = f'https://pubmedia.jw-api.org/GETPUBMEDIALINKS?issue={year}{month}&output=json'\
