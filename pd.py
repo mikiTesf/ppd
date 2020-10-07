a4ad4a988be07df8ef1caa6d2aed489f7ded4bc8
Mikyas Tesfamichael <mickyastesfamichael@gmail.com>
2020-10-07 13:53:49 +0300

    completed argument parsing

--- pd.py
+++ pd.py
@@ -4,7 +4,8 @@ import re
 from sys import argv
 
 
-args = argv
+options = dict()
+FILE_TYPES = ['PDF', 'EPUB', 'JWPUB', 'RTF']
 
 
 def zero_pad_month(month: str):
@@ -13,27 +14,73 @@ def zero_pad_month(month: str):
     return month
 
 
-today = datetime.today()
+def collect_options(args_list):
+    for arg in args_list:
+        match = re.match('year=(\\d{4})', arg)
+        if match:
+            options['year'] = match.group(1)
+            continue
 
-year = str(today.year)
-month = zero_pad_month(str(today.month))
-pub = None
-ftype = 'JWPUB'
-lang = 'AM'
-cont = False
+        match = re.match('month=(\\d{1,2})', arg)
+        if match:
+            options['month'] = zero_pad_month(match.group(1))
+            continue
 
+        match = re.match('ftype=([a-z]+)', arg, re.IGNORECASE)
+        if match:
+            ftype = match.group(1)
+            if ftype.upper() in FILE_TYPES:
+                options['ftype'] = ftype
+            continue
 
-def validate_args(
-        arg_year: str, arg_month: str, arg_pub: str,
-        arg_ftype: str, arg_lang: str, arg_cont: str):
-    # This line of comment has absolutely no purpose (love it or leave it)
-    match = re.match('year=(\\d{4})', arg_year)
-    year = match.group(1)
+        match = re.match('lang=([a-z]+)', arg, re.IGNORECASE)
+        if match:
+            options['lang'] = match.group(1)
+            continue
+
+        match = re.match('cont=(true|false)', arg, re.IGNORECASE)
+        if match:
+            options['cont'] = match.group(1)
+            continue
+
+        match = re.match('pub=([a-z]+)', arg, re.IGNORECASE)
+        if match:
+            options['pub'] = match.group(1)
+            continue
+
+    return options
 
 
 def get_resource_link(url: str):
     response = get(url)
     pass
+
+
+def download_publication(url: str):
+    pass
+
+
+args = argv[1:]
+
+# The first thing to check is if the user is trying to get help on how to use `pd`
+if (len(args) == 0) or ('-h' in args) or ('--help' in args):
+    # print(HELP)
+    exit(0)
+
+# If the execution made it this far, then start parsing for arguments, The following will be the defaults
+today = datetime.today()
+options['year'] = str(today.year)
+options['month'] = zero_pad_month(str(today.month))
+options['pub'] = None
+options['ftype'] = 'JWPUB'
+options['lang'] = 'AM'
+options['cont'] = 'false'
+
+options = collect_options(args)
+
+request_url = f"https://pubmedia.jw-api.org/GETPUBMEDIALINKS?issue={options['year']}{options['month']}&output=json" \
+              f"&pub={options['pub']}&fileformat={options['ftype']}%2CEPUB%2CJWPUB%2CRTF%2CTXT%2CBRL%2CBES%2CDAISY" \
+              f"&alllangs=0&langwritten={options['lang']}&txtCMSLang={options['lang']}"
 
 
 def download_publication(url: str):
